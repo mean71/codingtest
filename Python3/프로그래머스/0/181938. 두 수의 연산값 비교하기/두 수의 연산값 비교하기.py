ec66486e6d00bef7be4713dcdbdd18a721b9ec49
sleepotato <koreavally@gmail.com>
2024-07-23 16:42:46 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ Python3/프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,5 +1,5 @@
 def solution(a, b):
     answer = 0
     if all(1<=x<=10000 for x in[a,b]):
-        answer=max(map(int,(f'{a}{b}',2*a*b)))
+        answer=max(int(f'{a}{b}'),2*a*b)
     return answer
