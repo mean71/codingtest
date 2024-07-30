2904e6e5925a024f30843e736c44ac8220f397f6
Mean71 Park <koreavally@gmail.com>
2024-07-30 21:41:46 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ Python3/프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,8 +1,3 @@
 def solution(a, d, included):
-    adsum = 0
-    if 1<=a<=100 and 1<=d<=100 and 1<=len(included)<=100 and 1<=included.count(True):
-        for i,j in enumerate(included):
-            sumad=a+d*i
-            if j == True:
-                adsum+=sumad
-    return adsum
+    answer = 0
+    return answer
