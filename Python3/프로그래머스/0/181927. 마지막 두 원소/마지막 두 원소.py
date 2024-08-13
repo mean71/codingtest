b8d0678fe0159ef12efa7e66607b1ca5f7eba9fd
Mean71 Park <koreavally@gmail.com>
2024-08-13 21:10:48 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- Python3/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ Python3/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,4 +1,10 @@
-def solution(nlst):
-    if nlst[-2] < nlst[-1]: nlst.append(nlst[-1] - nlst[-2])
-    else: nlst.append(nlst[-1] * 2)
-    return nlst
+# def solution(nlst):
+#     if nlst[-2] < nlst[-1]: nlst.append(nlst[-1] - nlst[-2])
+#     else: nlst.append(nlst[-1] * 2)
+#     return nlst
+
+# def solution(l):
+#     l.append(l[-1]-l[-2] if l[-1]>l[-2] else l[-1]*2)
+#     return l
+
+solution = lambda l: l + [l[-1] - l[-2] if l[-1] > l[-2] else l[-1] * 2]
