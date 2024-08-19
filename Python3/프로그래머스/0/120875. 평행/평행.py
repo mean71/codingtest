9d1199fc6bfb7ae28e6d2f8a22b5229fa34afed8
Mean71 Park <koreavally@gmail.com>
2024-08-19 19:50:42 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,5 @@
+def solution(dots):
+    if (dots[0][0]-dots[1][0]) / (dots[0][1]-dots[1][1]) == (dots[2][0]-dots[3][0]) / (dots[2][1]-dots[3][1]): return 1
+    elif (dots[0][0]-dots[2][0]) / (dots[0][1]-dots[2][1]) == (dots[1][0]-dots[3][0]) / (dots[1][1]-dots[3][1]): return 1
+    elif (dots[0][0]-dots[3][0]) / (dots[0][1]-dots[3][1]) == (dots[1][0]-dots[2][0]) / (dots[1][1]-dots[2][1]): return 1
+    return 0
