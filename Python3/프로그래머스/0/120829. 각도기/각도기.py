93cb39ef38e74326a341a7335a978b0270c4d5c3
Mean71 Park <losleepotatool@gmail.com>
2024-08-26 16:07:14 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120829. 각도기/각도기.py
+++ Python3/프로그래머스/0/120829. 각도기/각도기.py
@@ -1,3 +1,2 @@
-solution=lambda r:1 if 0<r<90 else (2 if r==90 else (3 if 90<r<180 else (4 if r==180 else None)))
-# solution=lambda r:1 if 0<r<90 elif (2 if r==90 elif (3 if 90<r<180 elif (4 if r==180)))
-# else가 elif거나 else None을 생략하면 syntex?에러 발생
+# solution=lambda r: 1 if 0<r<90 else 2 if r==90 else 3 if 90<r<180 else 4
+solution=lambda r: 1 + (r==90) + 2*(90<r<180) + 3*(r==180)
