25186ac29115b9b09282872b11bab834e8a741d0
Mean71 Park <losleepotatool@gmail.com>
2024-08-27 22:12:03 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ Python3/프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,24 +1,21 @@
 def solution(a, b, c, d):
-    from collections import Counter
-    dice = [a, b, c, d]
-    count = Counter(dice)
-    if len(count) == 1:
-        return 1111 * dice[0]
-    if len(count) == 4:
-        return min(dice)
-    if len(count) == 3:
-        for value, cnt in count.items():
-            if cnt == 2:
-                pair_value = value
-                remaining_values = [x for x in dice if x != pair_value]
-                return remaining_values[0] * remaining_values[1]
-    if len(count) == 2:
-        pair_values = [value for value, cnt in count.items() if cnt == 2]
-        if len(pair_values) == 2:
-            p, q = pair_values
-            return (p + q) * abs(p - q)
-    if len(count) == 2 and any(cnt == 3 for cnt in count.values()):
-        three_of_a_kind_value = [value for value, cnt in count.items() if cnt == 3][0]
-        single_value = [value for value, cnt in count.items() if cnt == 1][0]
-        return (10 * three_of_a_kind_value + single_value) ** 2
-    return 0
+    x=[a,b,c,d]
+    if len(set(x))==1: return 1111*a
+    elif len(set(x))==4: return min(x)
+    elif a==b and c==d: return (a+c)*abs(a-c)
+    elif a==c and b==d: return (a+b)*abs(a-b)
+    elif a==d and b==c: return (a+b)*abs(a-b)
+    
+    elif len(set(x))==2:
+        return (10*[x[i] for i in range(4) if x.count(x[i])==3 ][0] + [x[i] for i in range(4) if x.count(x[i])==1][0])**2
+    elif len(set(x))==3:
+        y = [x[i] for i in range(4) if x.count(x[i])==1]
+        return y[0]*y[1]    
+'''
+a b c d min(x)
+p p p p 1111*p
+
+p p q q (p+q)*abs(p-q)
+p p p q (10*p+q)**2
+p p q r q*r
+'''
