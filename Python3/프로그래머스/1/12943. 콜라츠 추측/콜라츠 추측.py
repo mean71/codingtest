ab375154b38ba13d765825d19160df9503e44416
Mean71 Park <losleepotatool@gmail.com>
2024-09-02 22:21:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,9 @@
+def solution(x):
+    count = 0
+    if x==1: return 0
+    while x!=1:
+        if count==500: return -1
+        if x%2==0: x/=2
+        else: x=x*3+1
+        count+=1
+        if x==1: return count
