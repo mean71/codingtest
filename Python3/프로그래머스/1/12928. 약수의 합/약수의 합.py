37f86d5c63f9f156d5b594f255adca09bd398212
Mean71 Park <losleepotatool@gmail.com>
2024-09-07 14:40:25 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 10 MB -BaekjoonHub

--- Python3/프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ Python3/프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -5,7 +5,9 @@
 #             a += i+1
 #     return
 
-def solution(n):
-    if n==0: return 0
-    elif n==1: return 1
-    else: return sum([i+1 for i in range(1,n//2+1) if not n%(i+1)])+n+1
+# def solution(n):
+#     if n==0: return 0
+#     elif n==1: return 1
+#     else: return sum([i+1 for i in range(1,n//2+1) if not n%(i+1)])+n+1
+
+def solution(n): return sum([i for i in range(1,n//2+1) if not n%(i)]) + n
