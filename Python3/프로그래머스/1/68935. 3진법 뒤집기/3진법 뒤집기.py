3ffc28fd2b767c26558ba66e03afcbd2dc749ed7
Mean71 Park <losleepotatool@gmail.com>
2024-09-13 18:03:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ Python3/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,6 +1,6 @@
 def solution(n):
-    res = ''
-    while n > 0:
-        res = res+str(n%3)
+    tmp = ''
+    while n:
+        tmp += str(n%3)
         n//=3
-    return sum([3**i*int(j) for i,j in enumerate(res[::-1])])
+    return int(tmp, 3)
