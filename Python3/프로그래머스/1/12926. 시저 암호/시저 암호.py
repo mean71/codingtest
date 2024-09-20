f61aa7f41341e209b4d260bee9421d3dbed68a42
Mean71 Park <losleepotatool@gmail.com>
2024-09-20 19:40:01 +0900

    [level 1] Title: 시저 암호, Time: 1.33 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ Python3/프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,5 +1,5 @@
-solution = lambda s,n: ''.join([("ABCDEFGHIJKLMNOPQRSTUVWXYZ"["ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(i)+n if 25>"ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(i)+n else "ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(i)+n-26] if (i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ") else "abcdefghijklmnopqrstuvwxyz"["abcdefghijklmnopqrstuvwxyz".find(i)+n if 25>"abcdefghijklmnopqrstuvwxyz".find(i)+n else "abcdefghijklmnopqrstuvwxyz".find(i)+n-26] if i.isalpha() else i) for i in s])
+solution = lambda s,n: ''.join([("ABCDEFGHIJKLMNOPQRSTUVWXYZ"[("ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(i)+n)% 26] if (i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ") else "abcdefghijklmnopqrstuvwxyz"[("abcdefghijklmnopqrstuvwxyz".find(i)+n)% 26] if i.isalpha() else i) for i in s])
 # import string
 # up = string.ascii_uppercase
 # lo = string.ascii_lowercase
-# solution = lambda s,n: ''.join([(up[up.find(i)+n if 25>up.find(i)+n else up.find(i)+n-26] if (i in up) else lo[lo.find(i)+n if 25>lo.find(i)+n else lo.find(i)+n-26] if i.isalpha() else i) for i in s])
+# solution = lambda s,n: ''.join([(up[(up.find(i)+n)% 26] if (i in up) else lo[(lo.find(i)+n)% 26] if i.isalpha() else i) for i in s])
