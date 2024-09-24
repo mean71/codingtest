fa51c00732d7eabda8d919ad3690ff9eec854fdd
Mean71 Park <losleepotatool@gmail.com>
2024-09-24 22:28:51 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
+++ Python3/프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,11 +1,1 @@
-def solution(a, b):
-    answer = 0
-    if 1<=a<=6 and 1<=b<=6:
-        if (a+b)%2==0:
-            if a%2!=0:
-                answer = a**2 + b**2
-            else:
-                answer = abs(a-b)
-        else:
-            answer = 2*(a+b)
-    return answer
+solution=lambda a,b:(a**2+b**2 if x-1 else 2*(a+b))if (x:=a%2+b%2) else abs(a-b)
