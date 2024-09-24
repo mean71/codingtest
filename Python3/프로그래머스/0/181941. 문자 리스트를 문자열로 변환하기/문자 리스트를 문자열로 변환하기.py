56fab7ef612956cac6261d1d8252a8d784c940bb
Mean71 Park <losleepotatool@gmail.com>
2024-09-24 20:30:08 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.py
+++ Python3/프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.py
@@ -1,3 +1,1 @@
-def solution(arr):
-    while not(answer := '') and 1<=len(arr)<=200 and (all(arr[x].islower()) for x in range(len(arr))):answer = ''.join(arr);break
-    return answer
+solution=lambda a:''.join(a)
