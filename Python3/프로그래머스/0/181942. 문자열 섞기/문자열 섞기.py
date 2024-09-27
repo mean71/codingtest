6eeaaeab569c4f583afffe1509f2aa5e35dd4c62
Mean71 Park <losleepotatool@gmail.com>
2024-09-27 20:42:35 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ Python3/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,4 +1,1 @@
-def solution(str1, str2):
-    answer=''
-    while True and(1<=len(str1)==len(str2)<=10)and str1==str1.lower()and str2==str2.lower():answer+=''.join(str1[i]+str2[i]for i in range(0,len(str1)));break
-    return answer
+solution=lambda a,b:''.join(x+y for x,y in zip(a,b))
