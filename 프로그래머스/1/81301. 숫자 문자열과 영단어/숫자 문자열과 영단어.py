0cd94d282212c82f745e68b1d72336417bd94d9f
Mean71 Park <losleepotatool@gmail.com>
2024-10-02 21:46:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,13 +1,1 @@
-def solution(s):
-    n,t = '',''
-    numbers = {"zero":'0', "one":'1', "two":'2', "three":'3', "four":'4',
-               "five":'5', "six":'6', "seven":'7', "eight":'8', "nine":'9'}
-    for i,j in enumerate(s):
-        if j.isnumeric():
-            n += j
-        else:
-            t += j
-            if t in numbers:
-                n+= numbers[t]
-                t=''
-    return int(n)
+solution=lambda s:int([s:=s.replace(k,v)if k in s else s for k,v in{'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}.items()][-1])
