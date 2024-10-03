eae20d390391346b50666f352b00c9a27cc64e29
Mean71 Park <losleepotatool@gmail.com>
2024-10-03 17:26:57 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,5 +1,1 @@
-solution = lambda s: f"김서방은 {[i for i,x in enumerate(s) if 'Kim'== x][0]}에 있다"
-
-# def solution(s):
-#     for i,x in enumerate(s):
-#         if "Kim"== x:return f"김서방은 {i}에 있다"
+solution=lambda s:f"김서방은 {s.index('Kim')}에 있다"
