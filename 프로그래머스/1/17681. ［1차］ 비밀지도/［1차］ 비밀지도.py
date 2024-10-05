ac1e8dbbc7366d671732b3210820f2a5844430ad
Mean71 Park <losleepotatool@gmail.com>
2024-10-05 19:04:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.13 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,1 +1,1 @@
-solution=lambda n,x,y:list(map(lambda s:' '*(n-len(s))+s,[''.join('#'if int(s)else' 'for s in bin(i|j)[2:])for i,j in zip(x,y)]))
+solution=lambda n,x,y:[''.join('#'if int(s)else' 'for s in bin(i|j)[2:].zfill(n))for i,j in zip(x,y)]
