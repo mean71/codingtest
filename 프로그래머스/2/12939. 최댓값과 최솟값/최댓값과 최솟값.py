866b69c1ddebfc312a619fd07955c04e0032353d
Mean71 Park <losleepotatool@gmail.com>
2024-10-05 17:27:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,3 +1,2 @@
-def solution(s):
-    s = list(map(int,(s.strip().split(' '))))
-    return f'{min(s)} {max(s)}'
+# 최댓값과 최솟값
+solution=lambda s:f'{min(map(int,s.split()))} {max(map(int,s.split()))}'
