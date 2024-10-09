88bc34df8d42f294d8f880e9dba71f077ac1b59a
Mean71 Park <losleepotatool@gmail.com>
2024-10-10 02:05:00 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+N,M  = map(int,input().split())
+board = [input()for _ in range(N)]
+board1 = 'WBWBWBWB','BWBWBWBW'
+board2 = 'BWBWBWBW','WBWBWBWB'
+min_b = []
+
+for n in range(0,N-7):
+    for m in range(0,M-7):
+        b1,b2 = 0,0
+        for i in range(8):
+            for j in range(8):
+                temp = board[n+i][m+j]
+                b1 += temp!=board1[i%2][j]
+                b2 += temp!=board2[i%2][j]
+        min_b.append(min(b1,b2))
+print(min(min_b))
