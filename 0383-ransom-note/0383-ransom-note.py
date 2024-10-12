46ae6c50f7e79924df2f9297715165c8bee60d5c
Mean71 Park <losleepotatool@gmail.com>
2024-10-12 21:11:46 +0900

    Time: 45 ms (87.87%), Space: 16.7 MB (30.96%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for note in set(ransomNote):
+            if ransomNote.count(note) > magazine.count(note):
+                return False
+                
+        return True
