b3fa61b564a190e96bb4bde167b80876177778d0
Mean71 Park <losleepotatool@gmail.com>
2024-10-17 03:24:46 +0900

    [Silver IV] Title: 베스트셀러, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+book_n = {}
+bestseller = [0,'']
+if (N:=int(input()))==1:
+    print(input().strip())
+else:
+    for _ in range(N):
+        book = input().strip()
+        
+        if book not in book_n:
+            book_n[book] = 1
+        else:
+            book_n[book] += 1
+            
+        if bestseller[0] < book_n[book]:
+            bestseller = [book_n[book], book]
+        elif bestseller[0] == book_n[book] and bestseller[1] > book:
+            bestseller = [book_n[book], book]
+print(bestseller[1])
