cd960cca8e02466b8f44c4f42cb7db0dc2ef535d
Mean71 <losleepotatool@gmail.com>
2024-12-05 01:48:38 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,6 +1,5 @@
 def solution(lottos, Ns):
+    rank =[6,6,5,4,3,2,1]
     win_n = len(set(lottos)&set(Ns))
     zero = lottos.count(0)
-    x = x if (x:=(7 - win_n - zero)) < 6 else 6
-    y = y if (y:=(7 - win_n)) < 6 else 6
-    return [x,y]
+    return rank[win_n+zero], rank[win_n]
