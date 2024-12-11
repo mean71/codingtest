1ca01b1421ffd48fe0294a20a758b51b054ebb42
Mean71 <losleepotatool@gmail.com>
2024-12-11 15:39:25 +0900

    [level 0] Title: 소인수분해, Time: 0.49 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,1 +1,1 @@
-solution=lambda n:sorted({i for i in range(2, n+1) if n % i == 0 and all(i % j for j in range(2, int(i**0.5)+1))})
+solution=lambda N:[i for i in range(2,N//2+1)if N%i==0 and all(i%j for j in range(2,int(i**0.5)+1))]or[N]
