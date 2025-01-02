fb4f9c664f7fe63968bd871422f33980e3bc631e
Mean71 <losleepotatool@gmail.com>
2025-01-03 00:24:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 293.58 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,16 +1,13 @@
 def solution(elem):
-    # 결과를 저장할 집합 (중복 제거)
     res = set()
-    n = len(elem)
-
-    # 부분수열의 길이 1부터 len(elem)까지 반복
-    for length in range(1, n + 1):
-        # 시작점을 이동하며 부분합 계산
-        for start in range(n):
-            # 원형 배열 처리
-            sub_sum = sum(elem[start:start + length])  # 기본 부분합 계산
-            if start + length > n:  # 배열을 초과한 경우
-                sub_sum += sum(elem[:(start + length) % n])  # 초과된 부분 추가
-            res.add(sub_sum)  # 결과 집합에 추가
-
-    return len(res)  # 고유한 부분합의 개수 반환
+    length = len(elem)
+    
+    for n in range(1, length + 1):
+        sub_sum = sum(elem[0:n])
+        res.add(sub_sum)
+        
+        for s in range(1, length):
+            sub_sum += (elem[(s+n-1)%length] - elem[s])
+            res.add(sub_sum)
+            
+    return len(res)
