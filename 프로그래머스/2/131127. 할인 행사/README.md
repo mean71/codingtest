36ad16baebe2c900399b10f6774eecd125c14a5c
Mean71 <losleepotatool@gmail.com>
2025-01-09 04:13:59 +0900

    [level 2] Title: 할인 행사, Time: 66.12 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 17.2 MB, 시간: 40.86 ms
+메모리: 17.3 MB, 시간: 66.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 09일 01:21:32
+2025년 01월 09일 04:13:56
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,16 +1,11 @@
-def solution(want, n, discount):
-    want = {k:-v for k,v in zip(want, n)}
-    n = 0
-    for k in discount[:10]:
-        if k in want:
-            want[k] += 1
-        if not any(want.values()):
-            n += 1
-    for i in range(len(discount)-10):
-        if discount[i] in want:
+def solution(want, num, discount):
+    want = {k:v for k,v in zip(want, num)}
+    num = 0
+    for i in range(len(discount)):
+        if type(want.get(discount[i])) == int:
             want[discount[i]] -= 1
-        if discount[i+10] in want:
-            want[discount[i+10]] += 1
+        if i > 9 and type(want.get(discount[i-10])) == int:
+            want[discount[i-10]] += 1
         if not any(want.values()):
-            n += 1
-    return n
+            num += 1
+    return num
