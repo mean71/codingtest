d16badf46d535cede6b22a01014caded1e7df211
Mean71 <losleepotatool@gmail.com>
2025-01-09 04:17:36 +0900

    [level 2] Title: 할인 행사, Time: 42.79 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -2,9 +2,9 @@ def solution(want, num, discount):
     want = {k:v for k,v in zip(want, num)}
     num = 0
     for i in range(len(discount)):
-        if type(want.get(discount[i])) == int:
+        if discount[i] in want:
             want[discount[i]] -= 1
-        if i > 9 and type(want.get(discount[i-10])) == int:
+        if i > 9 and discount[i-10] in want:
             want[discount[i-10]] += 1
         if not any(want.values()):
             num += 1
