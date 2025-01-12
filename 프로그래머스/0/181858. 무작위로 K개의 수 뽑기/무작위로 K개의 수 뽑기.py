14a2fafcce674f7e11097e4be3036ba9c1e0ffcc
Mean71 <losleepotatool@gmail.com>
2025-01-12 13:24:06 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 2.45 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -1,10 +1,1 @@
-def solution(arr, k):
-    res = []
-    n_set = set()
-    
-    for n in arr:
-        if len(res) != k and n not in n_set:
-            res.append(n)
-            n_set.add(n)
-    
-    return res + [-1]*(k - len(res))
+solution=lambda arr,k:list(dict.fromkeys(arr))[:k] + [-1]*(k-len(set(arr)))
