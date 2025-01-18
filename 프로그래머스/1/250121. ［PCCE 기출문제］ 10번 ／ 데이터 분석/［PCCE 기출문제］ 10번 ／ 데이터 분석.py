c6491bafca980c4a6a68dba2fe798205af2b0c5c
Mean71 <losleepotatool@gmail.com>
2025-01-18 19:02:39 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,5 +1,3 @@
 def solution(data, ext, val_ext, sort_by):
     val = {"code":0, "date":1, "maximum":2, "remain":3}
-    ext = val[ext]
-    sort_by = val[sort_by]
-    return sorted(filter(lambda x:x[ext] < val_ext, data), key=lambda x:x[sort_by])
+    return sorted(filter(lambda x:x[val[ext]] < val_ext, data), key=lambda x:x[val[sort_by]])
