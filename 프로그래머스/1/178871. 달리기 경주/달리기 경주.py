af1bdd3c26a0234f182a7fb51b0089892bf1ed0c
Mean71 <losleepotatool@gmail.com>
2025-01-25 06:44:18 +0900

    [level 1] Title: 달리기 경주, Time: 1023.02 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,9 +1,10 @@
 def solution(players, callings):
-    rank = {p:i for i, p in enumerate(players)}
+    rank = {k:i for i,k in enumerate(players)}
     
     for call in callings:
         i = rank[call]
+        rank[players[i]] -= 1
+        rank[players[i-1]] += 1
         players[i], players[i-1] = players[i-1], players[i]
-        rank[players[i]], rank[players[i-1]] = rank[players[i-1]], rank[players[i]]
-        
+    
     return players
