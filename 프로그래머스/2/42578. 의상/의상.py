7962e019681ce85e009f8a5991865fb332cff33f
Mean71 <losleepotatool@gmail.com>
2025-01-26 23:33:12 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,3 +1,3 @@
 import math
 from collections import Counter
-solution=lambda clothes: math.prod([v+1 for v in Counter(map(lambda x:x[1], clothes)).values()]) - 1
+solution=lambda clothes: math.prod(v+1 for v in Counter(map(lambda x:x[1], clothes)).values()) - 1
