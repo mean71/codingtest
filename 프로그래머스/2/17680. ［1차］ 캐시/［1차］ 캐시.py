1c54f9ec2051b0f7adb5d1924db3d7dd80992a90
Mean71 <losleepotatool@gmail.com>
2025-01-30 08:26:06 +0900

    [level 2] Title: [1차] 캐시, Time: 48.00 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,18 +1,23 @@
 from collections import deque
 
 def solution(cacheSize, cities):
+    if not cacheSize: return len(cities)*5
+    cache_set = set()
     cache_que = deque(maxlen=cacheSize)
     res = 0
     
+    
     for city in cities:
         city = city.lower()
-        
-        if city in cache_que:
+        if city in cache_set:
             res += 1
             cache_que.remove(city)
             cache_que.append(city)
         else:
             res += 5
+            cache_set.add(city)
+            if len(cache_set) > cacheSize:
+                cache_set.discard(cache_que.popleft())
             cache_que.append(city)
-        
+
     return res
