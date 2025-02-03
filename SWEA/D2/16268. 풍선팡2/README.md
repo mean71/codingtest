486f40f14aad913626da2d8b7db6f14817c220a7
Mean71 <losleepotatool@gmail.com>
2025-02-03 18:24:04 +0900

    [D2] Title: 풍선팡2, Time: 105 ms, Memory: 73,856 KB -BaekjoonHub

---
+++ SWEA/D2/16268. 풍선팡2/README.md
@@ -0,0 +1,15 @@
+# [D2] 풍선팡2 - 16268 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYYlGU56XOkDFARc) 
+
+### 성능 요약
+
+메모리: 73,856 KB, 시간: 105 ms, 코드길이: 479 Bytes
+
+### 제출 일자
+
+2025-02-03 18:23
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.py
@@ -0,0 +1,15 @@
+dr = [0, 1, -1, 0, 0]
+dc = [0, 0, 0, 1, -1]
+
+for t in range(1, int(input()) + 1):
+    N,M = map(int, input().split())
+    balloons = [[0]*(M + 2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(N)] + [[0]*(M + 2)]
+    max_cnt = 0
+    
+    for r in range(1, N+1):
+        for c in range(1, M+1):
+            cnt = 0
+            for i in range(5):
+                cnt += balloons[r+dr[i]][c+dc[i]]
+            max_cnt = max(max_cnt, cnt)
+    print(f'#{t} {max_cnt}')
