58abadefaea1a6f42cafe431da49026561fac466
mean71 <losleepotatool@gmail.com>
2025-02-04 15:44:47 +0900

    정리 및 필기

--- PS Study/PyCoTest.py
+++ PS Study/PyCoTest.py
@@ -1,29 +1,5 @@
-def solution(num, hand):
-    res = ""
-    cur = ["*","#"]
-    for n in num:
-        if n in {1,4,7}:
-            res += "L"
-            cur[0] = n
-        elif n in {3,6,9}:
-            res += "R"
-            cur[1] = n
-        else:
-            ni = (2,5,8,0).index(n)
-            
-            if cur[0] in {2,5,8,0}:
-                l = abs(ni - (2,5,8,0).index(cur[0]))
-            else:
-                l = abs(ni - (1,4,7,"*").index(cur[0])) + 1
-            
-            if cur[1] in {2,5,8,0}:
-                r = abs(ni - (2,5,8,0).index(cur[1]))
-            else:
-                r = abs(ni - (3,6,9,"#").index(cur[1])) + 1
-            
-            res += ("L", "R")[l > r or l==r and hand=="right"]
-            cur[l > r or l==r and hand=="right"] = n
-    
-    return res
+def solution(citations):
+    return next(i for i,n in enumerate(sorted(citations+[0], reverse=True)) if i >= n )
+x = map(int, input().split())
 
-solution([1,3,4,5,8,2,1,4,5,9,5], "right") # "LRLLLRLLRRL"
+print(solution(list(x)))
