b27afd587274ffb8857531bde6f8449d2beac3f8
Mean71 <losleepotatool@gmail.com>
2025-02-04 15:55:14 +0900

    [level 2] Title: 전화번호 목록, Time: 123.62 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -4,11 +4,11 @@ def solution(phone_book):
         if phone_book[i+1].startswith(phone_book[i]):
             return False
     return True
-
-def solution(phone_book):
-    phone_book = set(phone_book)
-    for num in phone_book:
-        for i in range(len(num)):
-            if num[:i] in phone_book:
-                return False
-    return True
+# 비효율적
+# def solution(phone_book):
+#     phone_book = set(phone_book)
+#     for num in phone_book:
+#         for i in range(len(num)):
+#             if num[:i] in phone_book:
+#                 return False
+#     return True
