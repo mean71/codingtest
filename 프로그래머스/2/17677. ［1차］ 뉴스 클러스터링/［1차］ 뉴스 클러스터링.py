12e8eaa012bb5609960e9449c69fd1281c966694
Mean71 <losleepotatool@gmail.com>
2025-02-05 13:34:18 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.56 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -3,9 +3,8 @@ from collections import Counter
 def solution(str1, str2):
     str1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
     str2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
-    
-    if not (union := Counter(str1) | Counter(str2)):
+    if not str1 + str2:
         return 65536
-    intersection = Counter(str1) & Counter(str2)
-    
-    return int(65536 * sum(intersection.values()) / sum(union.values()))
+    str1 = Counter(str1)
+    str2 = Counter(str2)
+    return int(65536 * sum((str1&str2).values()) / sum((str1|str2).values()))
