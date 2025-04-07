90fe24e7fccef96114e0a2f63ef1b171bbae3cae
Mean71 <losleepotatool@gmail.com>
2025-04-08 00:39:43 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.42 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,9 +1,8 @@
 import re
 
+def parsing(file):
+    m =  re.match(r"(^\D+)(\d+)", file)
+    return m.group(1).lower(), int(m.group(2))
+
 def solution(files):
-    return sorted(files,
-                  key=lambda file: (
-                      (m := re.search(r"(?P<name>\D+)(?P<num>\d+)(?P<rest>.*)", file)).group("name").lower(),
-                      int(m.group("num"))
-                      )
-                  )
+    return sorted(files, key=parsing)
