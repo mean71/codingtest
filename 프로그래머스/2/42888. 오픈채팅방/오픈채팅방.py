614631fd062c3d9323470bd3cdbd14830214c95d
Mean71 <losleepotatool@gmail.com>
2025-04-14 06:07:41 +0900

    [level 2] Title: 오픈채팅방, Time: 105.12 ms, Memory: 51.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,19 @@
+def solution(record):
+    '''
+    args record(List[str]):
+    return (List[str]):
+    '''
+    id_name = {}
+    res = []
+    
+    for q in record:
+        q = q.split()
+        if q[0] == "Enter":
+            id_name[q[1]] = q[2]
+            res.append((q[1], "님이 들어왔습니다."))
+        elif q[0] == "Leave":
+            res.append((q[1], "님이 나갔습니다."))
+        elif q[0] == "Change":
+            id_name[q[1]] = q[2]
+    
+    return ["".join((id_name[_id], i_o)) for _id, i_o  in res]
