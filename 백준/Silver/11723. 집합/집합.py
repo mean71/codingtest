0e0057b8676564deacc19b2a86c890c7cc9bf104
Mean71 <losleepotatool@gmail.com>
2025-04-16 04:20:53 +0900

    [Silver V] Title: 집합, Time: 2916 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,40 +1,19 @@
 import sys
 input = sys.stdin.readline
-
-class Setting():
-    def __init__(self):
-        self.S = 0
-    
-    def __call__(self, cmd):
-        if cmd[0] == "add":
-            self.add(cmd[1])
-        elif cmd[0] == "remove":
-            self.remove(cmd[1])
-        elif cmd[0] == "check":
-            self.check(cmd[1])
-        elif cmd[0] == "toggle":
-            self.toggle(cmd[1])
-        elif cmd[0] == "all":
-            self.all()
-        elif cmd[0] == "empty":
-            self.empty()
-        else:
-            print("올바르지 않은 주문")
-    
-    def add(self, x):
-        self.S |= 1 << int(x)-1
-    def remove(self, x):
-        self.S &= ~(1 << int(x)-1)
-    def check(self, x):
-        print((self.S >> int(x)-1) & 1)
-    def toggle(self, x): # S.remove(x) if (x in S) else S.add(x)
-        self.S ^= 1 << int(x)-1
-    def all(self): # range(1,21)
-        self.S = (1 << 20) - 1
-    def empty(self): # 공집합
-        self.S = 0
-
-S = Setting()
-
+s = 0
 for _ in range(int(input())):
-    S(input().split())
+    cmd = input().split()
+    if cmd[0] == "add":
+        s |= 1 << int(cmd[1])-1
+    elif cmd[0] == "remove":
+        s &= ~(1 << int(cmd[1])-1)
+    elif cmd[0] == "check":
+        print((s >> int(cmd[1])-1) & 1)
+    elif cmd[0] == "toggle":
+        s ^= 1 << int(cmd[1])-1
+    elif cmd[0] == "all":
+        s = (1 << 20) - 1
+    elif cmd[0] == "empty":
+        s = 0
+    else:
+        print("올바르지 않은 주문")
