25fadcf669991100699385107f48b0840642ffdd
Mean71 <losleepotatool@gmail.com>
2025-05-01 23:25:52 +0900

    [Silver IV] Title: 진짜 메시지, Time: 272 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9324. 진짜 메시지/진짜 메시지.py
+++ 백준/Silver/9324. 진짜 메시지/진짜 메시지.py
@@ -6,16 +6,16 @@ def check(s):
     i = 0
     while i < len(s):
         c = s[i]
+        i += 1
         cnt[c] = cnt.get(c, 0) + 1
         
         if cnt[c] == 3:
-            if i == len(s)-1 or s[i+1] != c:
+            if i == len(s) or s[i] != c:
                 return "FAKE"
             cnt[c] = 0
             i += 1
-        i += 1
         
     return "OK"
 
 for _ in range(int(input())):
-    print(check(input()))
+    print(check(input().rstrip()))
