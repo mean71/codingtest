c05979ac02a2aece182c4751cabbe65d3f337f4e
Mean71 <losleepotatool@gmail.com>
2025-05-15 00:17:54 +0900

    [Gold V] Title: 강의실, Time: 528 ms, Memory: 48580 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -0,0 +1,22 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+start_heap = []
+end_heap = []
+room = 0
+
+for _ in range(int(input())):
+    n, start, end = map(int, input().split())
+    heapq.heappush(start_heap, (start, end))
+
+while start_heap:
+    start, end = heapq.heappop(start_heap)
+    
+    while end_heap and end_heap[0][0] <= start:
+        heapq.heappop(end_heap)
+    
+    heapq.heappush(end_heap, (end, start))
+    room = max(room, len(end_heap))
+
+print(room)
