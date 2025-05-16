cf26bc9c05a77d719cd662392bd4c1be6a65868c
Mean71 <losleepotatool@gmail.com>
2025-05-16 21:37:17 +0900

    [level 3] Title: 최고의 집합, Time: 0.67 ms, Memory: 9.92 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,3 +1,1 @@
-def solution(n:int, s:int) -> list:
-    div, n_mod_diff = s//n, n-s%n
-    return [-1] if s < n else [div + (i >= n_mod_diff) for i in range(n)]
+solution=lambda n,s: [-1]*(not (x:=s//n)) or (y:=n-s%n) and [x+(i >= y) for i in range(n)]
